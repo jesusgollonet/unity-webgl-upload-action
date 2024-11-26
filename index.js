cc03c2652e33d1f378369be85318a526f92f8b21
Jesus Gollonet <jesusgollonet@gmail.com>
2024-11-26 14:54:13 +0100

    Add input variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hi");
+import core from "@actions/core";
+
+const region = core.getInput("aws-region");
+const bucketName = core.getInput("bucket-name");
+const buildFolder = core.getInput("build-folder");
+const addressablesFolder = core.getInput("addressables-folder");
+
+console.log("wave hand");
+console.log(`Region: ${region}`);
+console.log(`Bucket Name: ${bucketName}`);
+console.log(`Build Folder: ${buildFolder}`);
+console.log(`Addressables Folder: ${addressablesFolder}`);

7696538f43809d7e30c349ea15bbab506b2dfab8
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-13 12:34:33 +0100

    Set env credentials explicitly

--- index.ts
+++ index.ts
@@ -4,7 +4,14 @@ import uploadDirectory from "./src/upload-directory";
 const region = core.getInput("aws-region");
 const bucketName = core.getInput("bucket-name");
 const buildFolder = core.getInput("build-folder");
+const accessKeyId = core.getInput("aws-access-key-id");
+const secretAccessKey = core.getInput("aws-secret-access-key");
 const addressablesFolder = core.getInput("addressables-folder");
+
+// env variables are not automatically set in the action
+process.env.AWS_REGION = region;
+process.env.AWS_ACCESS_KEY_ID = accessKeyId;
+process.env.AWS_SECRET_ACCESS_KEY = secretAccessKey;
 
 console.log(process.env);
 

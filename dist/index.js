a5439fd8c28e31375da2c715f76d6e1164111eaa
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-19 13:09:56 +0100

    Trace creds

--- dist/index.js
+++ dist/index.js
@@ -59676,10 +59676,12 @@ const buildFolder = core.getInput("build-folder");
 const accessKeyId = core.getInput("aws-access-key-id");
 const secretAccessKey = core.getInput("aws-secret-access-key");
 const addressablesFolder = core.getInput("addressables-folder");
+console.log("creds");
 // env variables are not automatically set in the action
 process.env.AWS_REGION = region;
 process.env.AWS_ACCESS_KEY_ID = accessKeyId;
 process.env.AWS_SECRET_ACCESS_KEY = secretAccessKey;
+console.log(process.env);
 console.log(`Region: ${region}`);
 console.log(`Bucket Name: ${bucketName}`);
 console.log(`Build Folder: ${buildFolder}`);

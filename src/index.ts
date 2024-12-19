42571f423d3daa7cbbd475b6ff308c97de767e75
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-19 13:19:47 +0100

    Remove default value for addressables folder

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,13 @@ const accessKeyId = core.getInput("aws-access-key-id");
 const secretAccessKey = core.getInput("aws-secret-access-key");
 const addressablesFolder = core.getInput("addressables-folder");
 
-console.log("creds");
+console.log("addressables folder", core.getInput("addressables-folder"));
 // env variables are not automatically set in the action
 process.env.AWS_REGION = region;
 
 process.env.AWS_ACCESS_KEY_ID = accessKeyId;
 process.env.AWS_SECRET_ACCESS_KEY = secretAccessKey;
 
-console.log(process.env);
 console.log(`Region: ${region}`);
 console.log(`Bucket Name: ${bucketName}`);
 console.log(`Build Folder: ${buildFolder}`);
